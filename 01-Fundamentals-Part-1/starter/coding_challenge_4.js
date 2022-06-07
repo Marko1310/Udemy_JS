5b4316ac848200876d8e4bdf1d6d5ae2e4d1aa7b
Marko <marko.cabo13@gmail.com>
2022-06-07 10:58:43 +0200

    finished coding challenge_04

--- 01-Fundamentals-Part-1/starter/coding_challenge_4.js
+++ 01-Fundamentals-Part-1/starter/coding_challenge_4.js
@@ -4,5 +4,5 @@ const tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill
 
 const value = bill + tip;
 
-console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${value}`);
 
+console.log(`The bill was ${bill}, the tip was ${tip}, and the total value is ${value}`);
