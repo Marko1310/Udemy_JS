ac1a09fbc96aaf6630ec6648370c43be20bd509d
Marko <marko.cabo13@gmail.com>
2022-06-20 20:33:46 +0200

    Implemented reset button

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,24 +1,10 @@
 'use strict';
 
-/*
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct Number😃';
-
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
-
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
+document.querySelector('.number').textContent = '?';
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -35,6 +21,8 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
 
     document.querySelector('.number').style.width = '30rem';
+
+    document.querySelector('.number').textContent = secretNumber;
 
     // When the guess is too high
   } else if (guess > secretNumber) {
@@ -64,4 +52,21 @@ document.querySelector('.check').addEventListener('click', function () {
   if (score === 0) {
     document.querySelector();
   }
+});
+
+//Reset button-----------------
+document.querySelector('.again').addEventListener('click', function () {
+  //Assign new secret number
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  //Change backround color
+  document.querySelector('body').style.backgroundColor = '#222';
+  //Reset the score
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  //Hide the secret number
+  document.querySelector('.number').textContent = '?';
+  //Remove the guess number
+  document.querySelector('.guess').value = '';
+  //Reset the message
+  document.querySelector('.message').textContent = 'Start guessing...';
 });
