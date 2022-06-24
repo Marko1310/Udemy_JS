5759d5290bb3ad8c8640c01b2238e3a3f85184b9
Marko <marko.cabo13@gmail.com>
2022-06-24 09:05:55 +0200

    Implement logic showing dice

---
+++ 07-Pig-Game/Pig-Game/script.js
@@ -0,0 +1,36 @@
+"use strict";
+
+//Define query Selectors
+const player1Box = document.querySelector(".player1Box");
+const player2Box = document.querySelector(".player2Box");
+const diceButton = document.querySelector(".rollDice");
+const dicePicture = document.querySelector(".dice");
+const images = [
+  "/pictures/dice-1.png",
+  "/pictures/dice-2.png",
+  "/pictures/dice-3.png",
+  "/pictures/dice-4.png",
+  "/pictures/dice-5.png",
+  "/pictures/dice-6.png",
+];
+
+//Define functions
+
+//Generate random dice number
+const dice = Math.round(Math.random() * (6 - 1) + 1);
+
+//Show according picture of a dice
+let image;
+for (let i = 1; i <= images.length; i++) {
+  if (dice === i) {
+    image = images[i - 1];
+  }
+}
+
+//Define the actions when the page is loaded
+window.onload = function () {
+  player1Box.classList.add("active");
+  dicePicture.src = image;
+};
+
+//Click the dice button
