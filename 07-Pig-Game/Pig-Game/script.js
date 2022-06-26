c2debf7a7aeab5c1362e33da22b70c269b9c9fb4
Marko <marko.cabo13@gmail.com>
2022-06-27 01:12:50 +0200

    ~v2.0: Restructured code

--- 07-Pig-Game/Pig-Game/script.js
+++ 07-Pig-Game/Pig-Game/script.js
@@ -7,16 +7,41 @@ const diceButton = document.querySelector(".rollDice");
 const dicePicture = document.querySelector(".dice");
 const holdButton = document.querySelector(".hold");
 const newGameButton = document.querySelector(".newGame");
-let player1CurrentScore = document.getElementById("currentScore1");
-let player2CurrentScore = document.getElementById("currentScore2");
+const player1Name = document.getElementById("1");
+const player2Name = document.getElementById("2");
+let player1CurrentScore = document.querySelector(".currentScore1");
+let player2CurrentScore = document.querySelector(".currentScore2");
 let player1TotalScore = document.querySelector(".player1TotalScore");
 let player2TotalScore = document.querySelector(".player2TotalScore");
 
-//Define scores
-let currentScore1 = 0;
-let totalScore1 = 0;
-let currentScore2 = 0;
-let totalScore2 = 0;
+//Define variables
+let activePlayer, scores, currentScore, totalScore;
+
+// Define initialization function
+const init = function () {
+  player1Box.classList.add("active");
+  player2Box.classList.remove("active");
+  activePlayer = 1;
+  scores = [0, 0];
+  currentScore = 0;
+  totalScore = 0;
+  player1CurrentScore.textContent = 0;
+  player2CurrentScore.textContent = 0;
+  player1TotalScore.textContent = 0;
+  player2TotalScore.textContent = 0;
+  dicePicture.classList.add("hidden");
+  diceButton.disabled = false;
+  holdButton.disabled = false;
+  player1Box.classList.remove("winnerBox");
+  player2Box.classList.remove("winnerBox");
+  document.getElementById("1").classList.remove("winnerPlayer");
+  document.getElementById("2").classList.remove("winnerPlayer");
+  player1Name.textContent = `PLAYER 1`;
+  player2Name.textContent = `PLAYER 2`;
+};
+
+// Call the function when starting the game
+init();
 
 //Click the dice button
 diceButton.addEventListener("click", function () {
@@ -25,86 +50,53 @@ diceButton.addEventListener("click", function () {
   dicePicture.src = `/pictures/dice-${number}.png`;
   dicePicture.classList.remove("hidden");
 
-  //If number is not equal to 1 and Player 1 is active: add points to Player 1
-  if (number !== 1 && player1Box.classList.contains("active")) {
-    currentScore1 += number;
-    player1CurrentScore.textContent = currentScore1;
+  //If number is not equal to 1: add points to active Player
+  if (number !== 1) {
+    currentScore += number;
+    document.querySelector(`.currentScore${activePlayer}`).textContent =
+      currentScore;
 
-    //If number is not equal to 1 and Player 2 is active: add points to Player 2
-  } else if (number !== 1 && player2Box.classList.contains("active")) {
-    currentScore2 += number;
-    player2CurrentScore.textContent = currentScore2;
-
-    //If number is equal to 1: change player
+    //If number is equal to 1: change player and set the current score to 0
   } else if (number === 1) {
-    if (player1Box.classList.contains("active")) {
-      currentScore1 = 0;
-      player1CurrentScore.textContent = currentScore1;
-      player1Box.classList.remove("active");
-      player2Box.classList.add("active");
-    } else if (player2Box.classList.contains("active")) {
-      currentScore2 = 0;
-      player2CurrentScore.textContent = currentScore2;
-      player2Box.classList.remove("active");
-      player1Box.classList.add("active");
-    }
+    currentScore = 0;
+    document.querySelector(`.currentScore${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 1 ? 2 : 1;
+    player1Box.classList.toggle("active");
+    player2Box.classList.toggle("active");
   }
 });
 
+//Hold Button
 holdButton.addEventListener("click", function () {
-  //If Player 1 is active: add to total score of Player 1
-  if (player1Box.classList.contains("active")) {
-    totalScore1 += currentScore1;
-    player1TotalScore.textContent = totalScore1;
-    //If Total score is >= 100, Player 1 wins ---- Implement change of DOM
-    if (totalScore1 >= 100) {
-      player1Box.classList.add("winnerBox");
-      document.getElementById("1").classList.add("winnerPlayer");
-      diceButton.disabled = true;
-      holdButton.disabled = true;
-    } else {
-      player1Box.classList.remove("active");
-      player2Box.classList.add("active");
-      currentScore1 = 0;
-      player1CurrentScore.textContent = currentScore1;
-    }
-
-    //If Player 2 is active: add to total score of Player 2
-  } else if (player2Box.classList.contains("active")) {
-    totalScore2 += currentScore2;
-    player2TotalScore.textContent = totalScore2;
-    //If Total score is >= 100, Player 2 wins ---- Implement change of DOM
-    if (totalScore2 >= 100) {
-      player2Box.classList.add("winnerBox");
-      document.getElementById("2").classList.add("winnerPlayer");
-      diceButton.disabled = true;
-      holdButton.disabled = true;
-    } else {
-      player2Box.classList.remove("active");
-      player1Box.classList.add("active");
-      currentScore2 = 0;
-      player2CurrentScore.textContent = currentScore2;
-    }
+  // Add current score to the active player
+  scores[activePlayer - 1] += currentScore;
+  // If score >= 100: add classe to the winning player and change the name of the title, and disable hold and dice buttons
+  if (scores[activePlayer - 1] >= 100) {
+    document
+      .querySelector(`.player${activePlayer}Box`)
+      .classList.add("winnerBox");
+    document.getElementById(`${activePlayer}`).classList.add("winnerPlayer");
+    document.getElementById(
+      `${activePlayer}`
+    ).textContent = `PLAYER ${activePlayer} WINS!`;
+    document.querySelector(`.player${activePlayer}TotalScore`).textContent =
+      scores[activePlayer - 1];
+    diceButton.disabled = true;
+    holdButton.disabled = true;
+    // Else add the value of the current score to the players total score and then change the player, and reset the value of current score
+  } else {
+    document.querySelector(`.player${activePlayer}TotalScore`).textContent =
+      scores[activePlayer - 1];
+    currentScore = 0;
+    document.querySelector(`.currentScore${activePlayer}`).textContent =
+      currentScore;
+    activePlayer = activePlayer === 1 ? 2 : 1;
+    player1Box.classList.toggle("active");
+    player2Box.classList.toggle("active");
+    currentScore = 0;
   }
 });
 
-//New game button: restart everything
-newGameButton.addEventListener("click", function () {
-  player1Box.classList.add("active");
-  player2Box.classList.remove("active");
-  currentScore1 = 0;
-  currentScore2 = 0;
-  totalScore1 = 0;
-  totalScore2 = 0;
-  player1CurrentScore.textContent = currentScore1;
-  player2CurrentScore.textContent = currentScore2;
-  player1TotalScore.textContent = totalScore1;
-  player2TotalScore.textContent = totalScore2;
-  dicePicture.classList.add("hidden");
-  diceButton.disabled = false;
-  holdButton.disabled = false;
-  player1Box.classList.remove("winnerBox");
-  player2Box.classList.remove("winnerBox");
-  document.getElementById("1").classList.remove("winnerPlayer");
-  document.getElementById("2").classList.removve("winnerPlayer");
-});
+//New game button: restart everything, call init function
+newGameButton.addEventListener("click", init);
