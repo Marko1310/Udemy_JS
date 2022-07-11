f1aa451e890cdc588c904f65a6f43ba8b8b11b9c
Marko <marko.cabo13@gmail.com>
2022-07-11 14:16:19 +0200

    Football betting app - coding challenge 3: Challenge completed

--- 09-Data-Structures-Operators/Football_betting_app/script.js
+++ 09-Data-Structures-Operators/Football_betting_app/script.js
@@ -104,13 +104,40 @@ for (let [team, odd] of Object.entries(game.odds)) {
   console.log(`Odd of ${teamStr} is ${odd}`);
 }
 
-// console.log(`Odd of victory ${game.team1} is ${game.odds.team1}`);
-// console.log(`Odd a draw is ${game.odds.x}`);
-// console.log(`Odd of victory ${game.team2} is ${game.odds.team2}`);
+// CHALLENGE #3
 
-// for (const [team, odd] of Object.entries(game.odds)) {
-//   teamStr = team;
-//   console.log(`Odd of ${teamStr} is `);
-// }
+const gameEvents = new Map([
+  [17, "⚽️ GOAL"],
+  [36, "🔁 Substitution"],
+  [47, "⚽️ GOAL"],
+  [61, "🔁 Substitution"],
+  [64, "🔶 Yellow card"],
+  [69, "🔴 Red card"],
+  [70, "🔁 Substitution"],
+  [72, "🔁 Substitution"],
+  [76, "⚽️ GOAL"],
+  [80, "⚽️ GOAL"],
+  [92, "🔶 Yellow card"],
+]);
 
-// console.log(Object.entries(game.odds));
+//. Create an array 'events' of the different game events that happened (no duplicates)
+const events = new Set();
+for (const [key, value1] of gameEvents) {
+  events.add(value1);
+}
+
+// 2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+gameEvents.delete(64);
+
+// 3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+// 4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+//       [FIRST HALF] 17: ⚽️ GOAL
+for (const [key, value] of gameEvents) {
+  key < 45
+    ? console.log(`[FIRST HALF] ${key}, ${value}`)
+    : console.log(`[SECOND HALF] ${key}, ${value}`);
+}
