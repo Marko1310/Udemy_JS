68e24e5f52208ee72d868d92bd624587a5b0127a
Marko <marko.cabo13@gmail.com>
2022-07-24 13:04:07 +0200

    Coding challenge 2: Completed

--- 11-Arrays-Bankist/Coding_challenges/script.js
+++ 11-Arrays-Bankist/Coding_challenges/script.js
@@ -13,7 +13,7 @@ Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia'
 HINT: Use tools from all lectures in this section so far 😉
 TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
 TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
-*/
+
 
 const dogsJulia = [3, 5, 2, 12, 7];
 const dogsKate = [4, 1, 15, 8, 3];
@@ -42,3 +42,33 @@ const checkDogs = function (arr1, arr2) {
 };
 
 checkDogs(dogsJulia, dogsKate);
+*/
+
+// Coding Challenge #2
+/* 
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4.
+2. Exclude all dogs that are less than 18 human years old (which is the same as keeping dogs that are at least 18 years old)
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages 😉)
+4. Run the function for both test datasets
+TEST DATA 1: [5, 2, 4, 1, 15, 8, 3]
+TEST DATA 2: [16, 6, 10, 5, 6, 1, 4]
+*/
+
+const calcAverageHumanAge = function (ages) {
+  const humanAge = ages.map(function (age) {
+    if (age <= 2) return age * 2;
+    else return 16 + age * 4;
+  });
+  const humanAgeOlder = humanAge.filter(function (age) {
+    return age >= 18;
+  });
+  const humanAverage =
+    humanAgeOlder.reduce(function (acc, age) {
+      return acc + age;
+    }) / humanAgeOlder.length;
+  return humanAverage;
+};
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
